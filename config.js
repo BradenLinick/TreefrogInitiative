2a1ce352733e5e1d55f65955d49f1df9e14d3a34
Braden Linick <blinick@alumni.nmu.edu>
2021-04-15 13:14:12 -0400

    as

--- config.js
+++ config.js
@@ -1,43 +1,74 @@
-import mockData from './mockData.js';
+// let jsondata = "";
+// let apiUrl = "./mockData.json"
 
-const oneCity = mockData.USW00003860;
+// async function getJson(url) {
+//     let response = await fetch(url);
+//     let data = await response.json()
+//     return data;
+// }
 
-new Chart(document.getElementById("line-chart"), {
-  type: 'line',
-  data: {
-    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-    datasets: [{ 
-        data: [86,114,106,106,107,111,133,221,783,2478],
-        label: "Africa",
-        borderColor: "#3e95cd",
-        fill: false
-      }, { 
-        data: [282,350,411,502,635,809,947,1402,3700,5267],
-        label: "Asia",
-        borderColor: "#8e5ea2",
-        fill: false
-      }, { 
-        data: [168,170,178,190,203,276,408,547,675,734],
-        label: "Europe",
-        borderColor: "#3cba9f",
-        fill: false
-      }, { 
-        data: [40,20,10,16,24,38,74,167,508,784],
-        label: "Latin America",
-        borderColor: "#e8c3b9",
-        fill: false
-      }, { 
-        data: [6,3,2,2,7,26,82,172,312,433],
-        label: "North America",
-        borderColor: "#c45850",
-        fill: false
+// async function main() {
+
+//     //OPTION 2
+//     jsondata = await getJson(apiUrl)
+//     console.log(jsondata);
+// }
+
+// main();
+
+let jsondata;
+fetch('./mockData.json')
+  .then(response => response.json())
+  .then(newData => {
+    console.log(newData)
+    jsondata = newData;
+  })
+  .catch(err => console.log(error));
+
+setTimeout(function(){ 
+  console.log(jsondata.USW00003860); 
+  const labels = [];
+  jsondata.USW00003860.forEach(item => labels.push(item.Data_Date));
+  console.log(labels)
+
+
+  new Chart(document.getElementById("line-chart"), {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: [{ 
+          data: [86,114,106,106,107,111,133,221,783,2478],
+          label: "Africa",
+          borderColor: "#3e95cd",
+          fill: false
+        }, { 
+          data: [282,350,411,502,635,809,947,1402,3700,5267],
+          label: "Asia",
+          borderColor: "#8e5ea2",
+          fill: false
+        }, { 
+          data: [168,170,178,190,203,276,408,547,675,734],
+          label: "Europe",
+          borderColor: "#3cba9f",
+          fill: false
+        }, { 
+          data: [40,20,10,16,24,38,74,167,508,784],
+          label: "Latin America",
+          borderColor: "#e8c3b9",
+          fill: false
+        }, { 
+          data: [6,3,2,2,7,26,82,172,312,433],
+          label: "North America",
+          borderColor: "#c45850",
+          fill: false
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'World population per region (in millions)'
       }
-    ]
-  },
-  options: {
-    title: {
-      display: true,
-      text: 'World population per region (in millions)'
     }
-  }
-});
+  });
+}, 3000);
