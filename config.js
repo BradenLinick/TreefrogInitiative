9daa09b8a6097023ab52730924b6d9b283cfbafc
Braden Linick <blinick@alumni.nmu.edu>
2021-04-15 14:36:10 -0400

    asdf

--- config.js
+++ config.js
@@ -1,21 +1,3 @@
-// let jsondata = "";
-// let apiUrl = "./mockData.json"
-
-// async function getJson(url) {
-//     let response = await fetch(url);
-//     let data = await response.json()
-//     return data;
-// }
-
-// async function main() {
-
-//     //OPTION 2
-//     jsondata = await getJson(apiUrl)
-//     console.log(jsondata);
-// }
-
-// main();
-
 let jsondata;
 fetch('./mockData.json')
   .then(response => response.json())
@@ -25,10 +7,11 @@ fetch('./mockData.json')
   })
   .catch(err => console.log(error));
 
-setTimeout(function(){ 
-  console.log(jsondata.USW00003860); 
+setTimeout(() => { 
+  console.log(jsondata.cityTemperature); 
   const labels = [];
-  jsondata.USW00003860.forEach(item => labels.push(item.Data_Date));
+  const cityTemp = jsondata.cityTemperature
+  cityTemp.forEach(item => labels.push(item.Data_Date));
   const tmaxDataset = {
     data: [],
     borderColor: "#3e95cd",
@@ -45,13 +28,10 @@ setTimeout(function(){
     fill: false,
   };
   const datasets = [];
-  jsondata.USW00003860.forEach(item => {
+  cityTemp.forEach(item => {
     tmaxDataset.data.push(item.tmax);
     tminDataset.data.push(item.tmin);
     prcpDataset.data.push(item.prcp);
-    // const newTmaxDataset = Object.create(tmaxDataset)
-    // const newTminDataset = Object.create(tminDataset)
-    // const newPrcpDataset = Object.create(prcpDataset)
   });
 
   datasets.push(tmaxDataset, tminDataset, prcpDataset)
@@ -62,30 +42,8 @@ setTimeout(function(){
   new Chart(document.getElementById("line-chart"), {
     type: 'line',
     data: {
-      labels: labels,
+      labels,
       datasets
-      // datasets: [{ 
-      //     data: [86,114,106,106,107,111,133,221,783,2478],
-      //     borderColor: "#3e95cd",
-      //     fill: false
-      //   }, { 
-      //     data: [282,350,411,502,635,809,947,1402,3700,5267],
-      //     borderColor: "#8e5ea2",
-      //     fill: false
-      //   }, { 
-      //     data: [168,170,178,190,203,276,408,547,675,734],
-      //     borderColor: "#3cba9f",
-      //     fill: false
-      //   }, { 
-      //     data: [40,20,10,16,24,38,74,167,508,784],
-      //     borderColor: "#e8c3b9",
-      //     fill: false
-      //   }, { 
-      //     data: [6,3,2,2,7,26,82,172,312,433],
-      //     borderColor: "#c45850",
-      //     fill: false
-      //   }
-      // ]
     },
     options: {
       title: {
