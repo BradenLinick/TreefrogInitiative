826c1246cba2bd3a93c1399f1eb0f3dca00e7cf0
Braden Linick <blinick@alumni.nmu.edu>
2021-04-16 14:13:19 -0400

    asdf

--- config.js
+++ config.js
@@ -115,11 +115,11 @@ require([
               myLineChart = new Chart(document.getElementById("bar-chart"), {
                 type: 'bar',
                 data: {
-                  labels: ["Travel", "Education", "Food", "Music", "Shopping"],
+                  labels: ["Travel", "Education", "Music", "Shopping"],
                   datasets: [
                     {
                       label: '',
-                      backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                      backgroundColor: ["#3e95cd", "#8e5ea2", "#e8c3b9", "#c45850"],
                       data: trendData
                     }
                   ]
